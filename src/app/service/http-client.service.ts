65eeae3edb44b9f39d139a273678ed108682b1cc
Michael Awad <michael.awad@ordina.nl>
2020-03-17 13:12:48 +0100

    fix: Formatting of imports

--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Candidate} from "../components/candidate/Candidate";
+import { Candidate } from "../components/candidate/Candidate";
 
 @Injectable({
   providedIn: 'root'
