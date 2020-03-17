496246abea8be7a9de6930391631a59ad09e772c
Michael Awad <michael.awad@ordina.nl>
2020-03-17 13:08:41 +0100

    fix: Formatting of imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {CodeEditorComponent} from './components/code-editor/code-editor.component';
-import {WelcomePageComponent} from './components/welcome-page/welcome-page.component';
-import {JavascriptTaskComponent} from './components/output/javascript-task/javascript-task.component';
-import {CandidateComponent} from "./components/candidate/candidate.component";
+import { NgModule} from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CodeEditorComponent } from './components/code-editor/code-editor.component';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { JavascriptTaskComponent } from './components/output/javascript-task/javascript-task.component';
+import { CandidateComponent } from "./components/candidate/candidate.component";
 
 const routes: Routes = [
   {
