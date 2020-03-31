8ff9b2095bc63b2305929dedcad3bd50fc329b03
dashakrolik <45757824+dashakrolik@users.noreply.github.com>
2020-03-31 13:21:36 +0200

    added generic http service functions

--- src/app/service/http/http-client.service.ts
+++ src/app/service/http/http-client.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { baseUrl } from '@shared/constants';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,23 +9,37 @@ import { baseUrl } from '@shared/constants';
 export class HttpClientService {
 
   constructor(
-    private httpClient: HttpClient
+    private http: HttpClient
   ) { }
 
-  getCandidates = () => this.httpClient.get<Candidate[]>(`${baseUrl}/candidate`);
+  get = (path: string): Observable<any> => this.http.get(`${baseUrl}/${path}`);
 
-  createCandidate = (candidate: Candidate) => this.httpClient.post<Candidate>(`${baseUrl}/candidate`, candidate);
+  post<T>(path: string, payload: T): Observable<any> {
+    return this.http.post(`${baseUrl}/${path}`, payload);
+  }
 
-  createSubmission = (submission: Submission) => this.httpClient.post<Submission>(`${baseUrl}/submission`, submission);
+  put<T>(path: string, payload: T): Observable<any> {
+    return this.http.put(`${baseUrl}/${path}`, payload);
+  }
+
+  delete = (path: string): Observable<any> => {
+    return this.http.delete(`${baseUrl}/${path}`);
+  }
+
+  getCandidates = () => this.http.get<Candidate[]>(`${baseUrl}/candidate`);
+
+  createCandidate = (candidate: Candidate) => this.http.post<Candidate>(`${baseUrl}/candidate`, candidate);
+
+  createSubmission = (submission: Submission) => this.http.post<Submission>(`${baseUrl}/submission`, submission);
 
   // The call to the backend. Linked to the endpoint to get all candidates.
-  getLanguage = () => this.httpClient.get<string>('http://localhost:8080/language');
+  getLanguage = () => this.http.get<string>('http://localhost:8080/language');
 
-  getLanguages = (language: string) => this.httpClient.post<string>(`${baseUrl}/language`, language);
+  getLanguages = (language: string) => this.http.post<string>(`${baseUrl}/language`, language);
 
   getTask(subTaskNumber: number) {
     // TODO: right now how you identify a task from the backend is by comparing the task description itself.
     //  Maybe find a way to identify them more easily like an id that is syncronized over both front and backend
-    return this.httpClient.get<number>(`${baseUrl}/api/v1.0/task` + subTaskNumber + '/');
+    return this.http.get<number>(`${baseUrl}/api/v1.0/task` + subTaskNumber + '/');
   }
 }
