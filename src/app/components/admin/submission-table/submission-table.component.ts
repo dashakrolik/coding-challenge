0a2a778338146f3a37e84947762fa46eaeac98f4
Richard van Leeuwen <richard.van.leeuwen@ordina.nl>
2020-04-07 16:17:36 +0200

    Added expansion to show non-placeholder answers correctly without bloating the table.

--- src/app/components/admin/submission-table/submission-table.component.ts
+++ src/app/components/admin/submission-table/submission-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
@@ -7,9 +8,20 @@ import { SubmissionService } from '@service/submission/submission.service';
 @Component({
   selector: 'app-submission-table',
   templateUrl: './submission-table.component.html',
-  styleUrls: ['./submission-table.component.css']
+  styleUrls: ['./submission-table.component.css'],
+  animations: [
+    trigger('expandAnswer', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class SubmissionTableComponent implements OnInit {
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'answer', 'taskId', 'languageId', 'correct'];
+
+  expandedAnswer: Submission | null;
 
   constructor(
     private submissionService: SubmissionService
@@ -20,10 +32,6 @@ export class SubmissionTableComponent implements OnInit {
   @ViewChild(MatTable) table: MatTable<Submission>;
   @Input() personId: number;
   dataSource: MatTableDataSource<Submission>;
-
-
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'answer', 'taskId', 'languageId', 'correct'];
 
   ngOnInit() {
     this.submissionService.getAllSubmissionsFromPerson(this.personId).subscribe(submissions => {
