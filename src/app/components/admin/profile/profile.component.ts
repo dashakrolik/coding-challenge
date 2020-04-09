afa9c4572f2a562af908aa9c84acd3007cdb421c
Richard van Leeuwen <richard.van.leeuwen@ordina.nl>
2020-04-09 15:50:13 +0200

    Fixed the form control in the typescript. Added ignore radix to tslint.

--- src/app/components/admin/profile/profile.component.ts
+++ src/app/components/admin/profile/profile.component.ts
@@ -1,72 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PersonService } from '@service/person/person.service';
-import { SubmissionService } from '@service/submission/submission.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
   person: Person;
-  submissions: Submission[];
-  personDetailsForm;
+  personDetailsForm: FormGroup;
+  getPersonSubscription: Subscription;
+  savePersonSubscription: Subscription;
+  deletePersonSubscription: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     private personService: PersonService,
-    private submissionsService: SubmissionService,
     private formBuilder: FormBuilder,
     private router: Router
-  ) {
-    this.personDetailsForm = this.formBuilder.group({
-      id: 0,
-      firstName: '',
-      lastName: '',
-      email: '',
-      role: '',
-      password: ''
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+    const id = parseInt(this.route.snapshot.params.id);
+    this.getPersonSubscription = this.personService.getPersonById(id).subscribe(person => {
+      this.person = person;
+      this.fillForm();
     });
   }
 
-  ngOnInit(): void {
-    let personId: number;
-    this.route.params.subscribe(event => personId = event.id);
-    if (history.state.person === undefined) {
-      this.personService.getPersonById(personId).subscribe(person => {
-        this.person = person;
-        this.setFormDetails();
-      });
-    } else {
-      this.person = history.state.person;
-      this.setFormDetails();
-    }
-    this.submissionsService.getAllSubmissionsFromPerson(personId).subscribe(submissions => this.submissions = submissions);
+  createForm = (): void => {
+    this.personDetailsForm = this.formBuilder.group({
+      id: [{ value: 0, disabled: true }],
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      role: [''],
+      password: ['']
+    });
   }
 
-  onSubmit = (personData): void => {
-    this.personService.updatePerson(personData.value).subscribe(savedPerson => {
+  fillForm = (): void => {
+    this.personDetailsForm.get('id').setValue(this.person.id);
+    this.personDetailsForm.get('firstName').setValue(this.person.firstName);
+    this.personDetailsForm.get('lastName').setValue(this.person.lastName);
+    this.personDetailsForm.get('email').setValue(this.person.email);
+    this.personDetailsForm.get('role').setValue(this.person.role);
+  }
+
+  savePerson = (personData): void => {
+    this.savePersonSubscription = this.personService.updatePerson(personData.value).subscribe(savedPerson => {
       this.person = savedPerson;
-      this.setFormDetails();
     });
   }
 
   deletePerson = (): void => {
-    this.personService.deletePerson(this.person.id).subscribe(() => {
-      this.router.navigate(['/admin']);
+    this.deletePersonSubscription = this.personService.deletePerson(this.person.id).subscribe(() => {
+      this.navigateToAdminPanel();
     });
   }
 
-  setFormDetails = (): void => {
-    this.personDetailsForm.get('id').value = this.person.id;
-    this.personDetailsForm.get('firstName').value = this.person.firstName;
-    this.personDetailsForm.get('lastName').value = this.person.lastName;
-    this.personDetailsForm.get('email').value = this.person.email;
-    this.personDetailsForm.get('role').value = this.person.role;
-    this.personDetailsForm.get('password').value = this.person.password;
-  }
+  navigateToAdminPanel = () => this.router.navigate(['/admin']);
 
-  toAdminPanel = () => this.router.navigate(['/admin']);
+  ngOnDestroy = () => {
+    this.getPersonSubscription.unsubscribe();
+    this.savePersonSubscription.unsubscribe();
+    this.deletePersonSubscription.unsubscribe();
+  }
 }
+
