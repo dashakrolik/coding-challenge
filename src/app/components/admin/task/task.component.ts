e6a783b226784f4fd154b73de1a713bae42b4880
Richard van Leeuwen <richard.van.leeuwen@ordina.nl>
2020-04-23 16:00:50 +0200

    fixed two components with selector app-task

--- src/app/components/admin/task/task.component.ts
+++ src/app/components/admin/task/task.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-task',
+  selector: 'app-admin-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
