2ba97d42de8389dd5f9199f10444d918e04bfd27
aartbitbucket <aartdenbraber@outlook.com>
2020-04-23 15:08:34 +0200

    Refactored the mat-select roles stuff

--- src/app/components/admin/profile/profile.component.ts
+++ src/app/components/admin/profile/profile.component.ts
@@ -17,9 +17,10 @@ export class ProfileComponent implements OnInit {
   person: IPerson;
   personDetailsForm: FormGroup;
   // TODO: get roles from backend
-  roles: string[];
   allRoles: IRole[];
-
+  roles: IRole[];
+  selectedRoles: string[];
+  
   constructor(
     private route: ActivatedRoute,
     private personService: PersonService,
@@ -28,20 +29,25 @@ export class ProfileComponent implements OnInit {
     private dialogService: DialogService,
     private roleService: RoleService,
   ) { }
-
+    
   ngOnInit(): void {
     this.createForm();
+    
     const id = parseInt(this.route.snapshot.params.id);
     this.personService.getPersonById(id).pipe(take(1)).subscribe(person => {
       this.person = person;
+      
+      // mat-select only allows a string-array to pre-select values. Ugly, but yeah ¯\_(ツ)_/¯
+      this.selectedRoles = this.person.roles.map(role => role.name);
+      
       this.fillForm();
     });
+    
     this.roleService.getRoles().pipe(take(1)).subscribe(allRoles => {
       this.allRoles = allRoles;
-      this.roles = allRoles.map(role => role.name);
     });
   }
-
+  
   createForm = (): void => {
     this.personDetailsForm = this.formBuilder.group({
       id: [{ value: 0, disabled: true }],
@@ -52,55 +58,46 @@ export class ProfileComponent implements OnInit {
       password: ['']
     });
   }
-
+  
   fillForm = (): void => {
     this.personDetailsForm.patchValue({ ...this.person });
-    this.personDetailsForm.get('roles').setValue(this.person.roles.map(role => role.name));
+    this.personDetailsForm.get('roles').setValue(this.selectedRoles); // set the roles which should be selected
     this.personDetailsForm.get('password').setValue('');
   }
-
-  /**
-   * Returns array of strings
-   */
-  getRolesStrs = (person: IPerson): string[] => {
-    return person.roles.map(role => role.name);
-  }
-
+  
   savePerson = () => {
     const data = {
       title: 'Save personDetails',
       message: 'Are you sure you want to save these changes?'
     };
     this.dialogService.openOkCancel(data)
-      .then(() => {
-        this.savePersonToBackend();
-      }).catch(() => { });
+      .then(this.savePersonToBackend).catch(() => { });
   }
-
+  
   savePersonToBackend = (): void => {
-    const personId = this.person.id;
-    this.person = this.personDetailsForm.value;
-    this.person.id = personId;
+    Object.assign(this.person, this.personDetailsForm.value);
+    
+    // Retrieve the right roles by checking the mat-select result
     this.person.roles = this.allRoles.filter(role => {
-      return this.person.roles.includes(role.name as string) as boolean;
-    }
-    );
+      return this.selectedRoles.includes(role.name);
+    });
+    
     this.personService.updatePerson(this.person).pipe(take(1)).subscribe(savedPerson => {
       this.person = savedPerson;
     });
   }
-
+  
   deletePerson = () => {
     const data = {
       title: 'Delete person',
       message: 'Are you sure you want to delete this person?<br/>This also deletes all their submissions.'
     };
     this.dialogService.openOkCancel(data)
-      .then(() => {
-        this.deletePersonOnBackend();
-      }).catch(() => { });
+      .then(this.deletePersonOnBackend).catch(() => { 
+        // when the user cancels, do nothing
+      });
   }
-
+  
   deletePersonOnBackend = (): void => {
     const idControl: AbstractControl = this.personDetailsForm.get('id');
     idControl.enable();
@@ -109,7 +106,10 @@ export class ProfileComponent implements OnInit {
       idControl.disable();
     });
   }
-
+  
   navigateToAdminPanel = () => this.router.navigate(['/admin']);
-
+  
 }
+
+
+  
