265dda5d1bd80a1a4d6e1e72ee6e80418eaf7d90
Richard van Leeuwen <richard.van.leeuwen@ordina.nl>
2020-04-24 10:26:10 +0200

    Sanitized the email link so angular doesn't prepend "unsafe:" to the anymore.

--- src/app/components/leaderboard/card/card.component.ts
+++ src/app/components/leaderboard/card/card.component.ts
@@ -1,10 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
   @Input('person') person: Person;
+  getMailLink = () => {
+    const url = 'mailto:' + this.person.username + '?subject=Hello from the coding challenge.';
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  constructor(private sanitizer: DomSanitizer) { }
 }
