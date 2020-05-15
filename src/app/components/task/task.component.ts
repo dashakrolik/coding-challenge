5035fec240e46aa0f885de1c313030ee5051ce15
Grabot <SanderKools@gmail.com>
2020-05-15 18:57:17 +0200

    changed the code to work with the new submissionResponse from the backend

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -69,13 +69,6 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    // TODO check if necessary
-    // this.editor.setOptions({
-    //   animatedScroll: true,
-    //   showPrintMargin: false,
-    //   tabSize: 2,
-    //   useSoftTabs: true,
-    // });
   }
 
   ngOnDestroy() {
@@ -95,10 +88,8 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
       runningTime: 0
     };
     const kernelId = this.tokenStorageService.getKernelId(this.selectedLanguage.language);
-    console.log('evaluating code');
     await this.submissionService.runCode(runCodeSubmission, kernelId).toPromise().then(response => {
       this.codeResult = '';
-      console.log("kernelId");
       this.tokenStorageService.setKernelId(response.kernelId, this.selectedLanguage.language);
       response.jupyterResponses.forEach(line => {
         console.log(line);
@@ -112,24 +103,6 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
       });
     });
     console.log(this.codeResult);
-  }
-
-  handleSuccessfulResponseRunCode = (response): void => {
-    // First we clear the current output for the new input
-    // TODO This is not called anymore. fill the 'codeResult' in the 'evaluateCode' function
-    this.codeResult = '';
-    console.log('run2', response);
-    response.forEach(element => {
-      // We check if it is not an error than we show the output, otherwise we show the error.
-      console.log(element);
-      if (element.errorType === null) {
-        this.codeResult += element.contentValue;
-      } else {
-        this.codeResult += element.errorType;
-        this.codeResult += '\n';
-        this.codeResult += element.errorValue;
-      }
-    });
   }
 
   loginWindow(content: ComponentType<SubscribeComponent>) {
@@ -155,7 +128,8 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
       this.submissionService.createSubmission(submission, kernelId).subscribe(
         response => {
           this.codeResult = '';
-          this.tests = response;
+          this.tokenStorageService.setKernelId(response.kernelId, this.selectedLanguage.language);
+          this.tests = response.testResultsTest;
           console.log('successful post message create submission');
         }
       );
@@ -168,11 +142,6 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
     this.languageSubscription = this.languageService.getLanguagesMap().subscribe((languagesMap) => {
       this.selectedLanguage = languagesMap.get(languageParam);
       this.setBoilerPlateCode();
-      // if (!this.selectedLanguage) {
-      //   throw new Error(`No such language: ${languageParam}.`);
-      // } else {
-      //   this.setBoilerPlateCode();
-      // }
     });
   }
 
@@ -250,6 +219,7 @@ export class TaskComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   finishedCodeChallenge = () => {
+    // TODO: add some functionality if the user has finished the code challenge, like showing score or going to leaderboard or profile.
     console.log('The code challenge is completed');
   }
 
