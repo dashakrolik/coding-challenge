8c9fbf7f981f8242b419347f6f81f7e88b92e55d
M.D. van Es <es@mdworld.nl>
2020-06-22 16:36:42 +0200

    Add unit tests for logged in

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,6 +6,7 @@ import { Location } from "@angular/common";
 import { OverlayService } from '@services/overlay/overlay.service';
 
 import { AppComponent } from './app.component';
+import { TokenStorageService } from '@services/token/token-storage.service';
 
 const WELCOME_PAGE_STUB_TITLE = 'welcome page stub';
 
@@ -15,10 +16,17 @@ class WelcomePageStubComponent { }
 @Component({ selector: 'app-leaderboard', template: '<h1>leaderboard stub</h1>' })
 class LeaderboardStubComponent { }
 
+@Component({ selector: 'app-profile', template: '<h1>profile stub</h1>' })
+class ProfileStubComponent { }
+
 const mockRoutes: Routes = [
   {
     path: '',
     component: WelcomePageStubComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileStubComponent,
   },
   {
     path: 'leaderboard',
@@ -30,59 +38,111 @@ fdescribe('AppComponent', () => {
   let location: Location;
   let router: Router;
   let OverlayServiceSpy: jasmine.SpyObj<{}>;
+  let TokenStorageServiceSpy: jasmine.SpyObj<{ isUserLoggedIn: () => boolean }>;
   let fixture;
 
-  beforeEach(async(() => {
+  describe('when logged out', () => {
 
-    TestBed.configureTestingModule({
-      imports: [
-        RouterModule,
-        RouterTestingModule.withRoutes(mockRoutes)
-      ],
-      declarations: [
-        AppComponent, WelcomePageStubComponent, LeaderboardStubComponent
-      ],
-      providers: [AppComponent, { provide: OverlayService, useValue: OverlayServiceSpy }]
-    }).compileComponents();
+    beforeEach(async(() => {
+      TokenStorageServiceSpy = jasmine.createSpyObj('TokenStorageService', ['isUserLoggedIn']);
+      TokenStorageServiceSpy.isUserLoggedIn.and.returnValue(false);
 
-    router = TestBed.get(Router);
-    location = TestBed.get(Location);
+      TestBed.configureTestingModule({
+        imports: [
+          RouterModule,
+          RouterTestingModule.withRoutes(mockRoutes)
+        ],
+        declarations: [
+          AppComponent, WelcomePageStubComponent, LeaderboardStubComponent
+        ],
+        providers: [AppComponent, { provide: OverlayService, useValue: OverlayServiceSpy }, { provide: TokenStorageService, useValue: TokenStorageServiceSpy }]
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(AppComponent);
-    router.initialNavigation();
-  }));
+      router = TestBed.get(Router);
+      location = TestBed.get(Location);
 
-  it('should create the app', () => {
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+      fixture = TestBed.createComponent(AppComponent);
+      router.initialNavigation();
+    }));
+
+    it('should create the app', () => {
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
+
+    it(`has the title 'code-challenge'`, () => {
+      const app = fixture.componentInstance;
+      expect(app.title).toEqual('code-challenge');
+    });
+
+    it('renders Login and Leaderboard buttons', () => {
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement;
+      const [firstButton, secondButton] = compiled.querySelectorAll('button');
+      expect(firstButton.textContent).toContain('Login');
+      expect(secondButton.textContent).toContain('Leaderboard');
+    });
+
+    it('loads the default route', () => {
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain(WELCOME_PAGE_STUB_TITLE);
+    });
+
+    it('navigates to /leaderboard when Leaderboard button is clicked', fakeAsync(() => {
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement;
+      compiled.querySelector('button[name=leaderboard]').click();
+      tick();
+      expect(location.path()).toBe("/leaderboard")
+    }));
+
+    it('calls isUserLoggedIn from the template via the checkLogin method', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      expect(TokenStorageServiceSpy.isUserLoggedIn).toHaveBeenCalledTimes(6);
+    });
   });
 
-  it(`has the title 'code-challenge'`, () => {
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('code-challenge');
+  describe('when logged in', () => {
+
+    beforeEach(async(() => {
+      TokenStorageServiceSpy = jasmine.createSpyObj('TokenStorageService', ['isUserLoggedIn']);
+      TokenStorageServiceSpy.isUserLoggedIn.and.returnValue(true);
+
+      TestBed.configureTestingModule({
+        imports: [
+          RouterModule,
+          RouterTestingModule.withRoutes(mockRoutes)
+        ],
+        declarations: [
+          AppComponent, WelcomePageStubComponent, LeaderboardStubComponent
+        ],
+        providers: [AppComponent, { provide: OverlayService, useValue: OverlayServiceSpy }, { provide: TokenStorageService, useValue: TokenStorageServiceSpy }]
+      }).compileComponents();
+
+      router = TestBed.get(Router);
+      location = TestBed.get(Location);
+
+      fixture = TestBed.createComponent(AppComponent);
+      router.initialNavigation();
+    }));
+
+    it('renders My Account, Logout, and Leaderboard buttons', () => {
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement;
+      const [firstButton, secondButton, thirdButton] = compiled.querySelectorAll('button');
+      expect(firstButton.textContent).toContain('My Account');
+      expect(secondButton.textContent).toContain('Logout');
+      expect(thirdButton.textContent).toContain('Leaderboard');
+    });
+
+    it('navigates to /profile when My Account button is clicked', fakeAsync(() => {
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement;
+      compiled.querySelector('button[name=my-account]').click();
+      tick();
+      expect(location.path()).toBe("/profile")
+    }));
   });
-
-  it('renders Login and Leaderboard buttons', () => {
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    const [firstButton, secondButton] = compiled.querySelectorAll('button');
-    expect(firstButton.textContent).toContain('Login');
-    expect(secondButton.textContent).toContain('Leaderboard');
-  });
-
-  it('loads the default route', () => {
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(WELCOME_PAGE_STUB_TITLE);
-  });
-
-  it('navigates to /leaderboard when Leaderboard button is clicked', fakeAsync(() => {
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    compiled.querySelector('button[name=leaderboard]').click();
-    tick();
-    expect(location.path()).toBe("/leaderboard")
-  }));
-
-  // TODO test checkLogin, myAccount, logout, loginWIndow
 });
