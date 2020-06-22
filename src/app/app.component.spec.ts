9cb31e305ce9d117aa784e574b494ffd07f5e81b
M.D. van Es <es@mdworld.nl>
2020-06-22 16:14:13 +0200

    Add unit tests for default route, navigation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,41 +1,88 @@
-import { async, TestBed } from '@angular/core/testing';
+import { async, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { Component, NgZone } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
-
-import { AppComponent } from './app.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { Location } from "@angular/common";
 import { OverlayService } from '@services/overlay/overlay.service';
 
-describe('AppComponent', () => {
+import { AppComponent } from './app.component';
+
+const WELCOME_PAGE_STUB_TITLE = 'welcome page stub';
+
+@Component({ selector: 'app-welcome-page', template: `<h1>${WELCOME_PAGE_STUB_TITLE}</h1>` })
+class WelcomePageStubComponent { }
+
+@Component({ selector: 'app-leaderboard', template: '<h1>leaderboard stub</h1>' })
+class LeaderboardStubComponent { }
+
+const mockRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomePageStubComponent
+  },
+  {
+    path: 'leaderboard',
+    component: LeaderboardStubComponent,
+  },
+];
+
+fdescribe('AppComponent', () => {
+  let location: Location;
+  let router: Router;
   let OverlayServiceSpy: jasmine.SpyObj<{}>;
+  let fixture;
 
   beforeEach(async(() => {
 
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterModule,
+        RouterTestingModule.withRoutes(mockRoutes)
       ],
       declarations: [
-        AppComponent
+        AppComponent, WelcomePageStubComponent, LeaderboardStubComponent
       ],
       providers: [AppComponent, { provide: OverlayService, useValue: OverlayServiceSpy }]
     }).compileComponents();
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'code-challenge'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`has the title 'code-challenge'`, () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('code-challenge');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('code-challenge app is running!');
-  // });
+  it('renders Login and Leaderboard buttons', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    const [firstButton, secondButton] = compiled.querySelectorAll('button');
+    expect(firstButton.textContent).toContain('Login');
+    expect(secondButton.textContent).toContain('Leaderboard');
+  });
+
+  it('loads the default route', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(WELCOME_PAGE_STUB_TITLE);
+  });
+
+  it('navigates to /leaderboard when Leaderboard button is clicked', fakeAsync(() => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    compiled.querySelector('button[name=leaderboard]').click();
+    tick();
+    expect(location.path()).toBe("/leaderboard")
+  }));
+
+  // TODO test checkLogin, myAccount, logout, loginWIndow
 });
