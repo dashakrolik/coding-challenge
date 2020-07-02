8fcf3c06fb15979b7b4e62ee6844f9affc7d7462
M.D. van Es <es@mdworld.nl>
2020-07-02 11:58:08 +0200

    Mock HttpClient in Feedback service

--- src/app/services/feedback/feedback.service.spec.ts
+++ src/app/services/feedback/feedback.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
 import { FeedbackService } from './feedback.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('FeedbackService', () => {
   let service: FeedbackService;
+  let HttpClientSpy;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    HttpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
+    TestBed.configureTestingModule({ providers: [FeedbackService, { provide: HttpClient, useValue: HttpClientSpy }] });
     service = TestBed.inject(FeedbackService);
   });
 
