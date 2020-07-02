7300c1188e8208ec816f9ad30859a586620f9cd0
M.D. van Es <es@mdworld.nl>
2020-07-02 13:33:49 +0200

    Mock getFeedback in FeedbackComponent spec

--- src/app/components/admin/feedback/feedback.component.spec.ts
+++ src/app/components/admin/feedback/feedback.component.spec.ts
@@ -1,16 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FeedbackComponent } from './feedback.component';
+import { FeedbackService } from '@services/feedback/feedback.service';
+import { of } from 'rxjs';
+
+const mockFeedback: IFeedback = { email: "", feedback: "", id: 1 };
 
 describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
+  let FeedbackServiceSpy;
 
   beforeEach(async(() => {
+    FeedbackServiceSpy = jasmine.createSpyObj('FeedbackService', ['getFeedback']);
+    FeedbackServiceSpy.getFeedback.and.returnValue(of([mockFeedback]));
     TestBed.configureTestingModule({
-      declarations: [ FeedbackComponent ]
+      declarations: [FeedbackComponent],
+      providers: [{
+        provide: FeedbackService,
+        useValue: FeedbackServiceSpy
+      }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +33,8 @@ describe('FeedbackComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('retrieves the feedback on init', () => {
+    expect(FeedbackServiceSpy.getFeedback).toHaveBeenCalledTimes(1);
+  })
 });
